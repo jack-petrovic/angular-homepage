e35600e88df46dc4f83ce2a3cd6632123fe5bb38
Admin <jack.petrovic@outlook.com>
2025-09-17 17:34:08 +0200

    add search bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,7 +7,7 @@ import { Home } from './home/home';
   template: `
     <main>
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        <img class="brand-logo" src="logo.png" alt="logo" />
       </header>
       <section class="content">
         <app-home></app-home>
