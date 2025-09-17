7c104310a1e001f32e13c6f2bdf686eaf766b214
Admin <jack.petrovic@outlook.com>
2025-09-17 18:28:28 +0200

    add an interpolation

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from "../housing-location/housing-location";
+import { HousingLocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
+  styleUrls: ['./home.css'],
   template: `
     <section>
       <form>
@@ -9,9 +12,23 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location [housingLocation]="housingLocationList"></app-housing-location>
+    </section>
   `,
-  styleUrls: ['./home.css'],
+  imports: [HousingLocation],
 })
 export class Home {
-  
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocationList: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Housing Location',
+    city: 'Test City',
+    state: 'Test State',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
