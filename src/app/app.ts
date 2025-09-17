a2457f68d2189e41812bb2749e8b2a73620b56c3
Admin <jack.petrovic@outlook.com>
2025-09-17 19:34:38 +0200

    add routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { Home } from './home/home';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [Home, RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="logo.png" alt="logo" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="logo.png" alt="logo" />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
