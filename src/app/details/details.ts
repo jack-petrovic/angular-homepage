d2891c46338e54832ea2d8129f53ef112f409835
Admin <jack.petrovic@outlook.com>
2025-09-18 15:03:41 +0200

    integrate details page

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing';
+import { HousingLocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   imports: [],
-  templateUrl: './details.html',
+  template: `
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+        crossorigin
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Dose this location have wifi: {{ housingLocation?.wifi }}</li>
+          <li>Dose this location have laundry: {{ housingLocation?.laundry }}</li>
+        </ul>
+      </section>
+    </article>
+  `,
   styleUrl: './details.css'
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocationInfo | undefined;
 
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
