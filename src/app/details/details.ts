81bb31b447870b5a0da526f84f15d2b1cd19c447
Admin <jack.petrovic@outlook.com>
2025-09-24 10:16:14 +0200

    rename service component

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HousingService } from '../housing';
+import { HousingService } from '../housing.service';
 import { HousingLocationInfo } from '../housinglocation';
 
 @Component({
